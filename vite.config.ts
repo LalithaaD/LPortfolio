6678fd7fecd315505b1726dc907dbe2f44ffc622
Lalitha Madhuri Divakaruni <lalithamadhuri3@gmail.com>
2025-11-25 16:40:13 -0500

    Setup GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/", // required for GitHub Pages
+  base: "/LPortfolio/", // required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
